e48cf49615cfb9610711a89df15434c046e8a1bc
ptteya <petq_1002@abv.bg>
2023-01-29 16:08:02 +0200

    Setup express and env variables

---
+++ 05-Workshop ExpressJS and Templating 2/src/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const config = require('./config');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Home page');
+});
+
+app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`));
