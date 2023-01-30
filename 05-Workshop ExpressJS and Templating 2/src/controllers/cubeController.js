11736d7aeaa43a62c54e42767ce84511b16044f7
ptteya <petq_1002@abv.bg>
2023-01-30 18:11:13 +0200

    Add details page

--- 05-Workshop ExpressJS and Templating 2/src/controllers/cubeController.js
+++ 05-Workshop ExpressJS and Templating 2/src/controllers/cubeController.js
@@ -1,4 +1,5 @@
 const Cube = require('../models/Cube');
+const db = require('../db.json');
 
 exports.getCreateCube = (req, res) => {
     res.render('create');
@@ -13,3 +14,13 @@ exports.postCreateCube = (req, res) => {
 
     res.redirect('/');
 };
+
+exports.getDetails = (req, res) => {
+    let cube = db.cubes.find(x => x.id === req.params.cubeId);
+
+    if (!cube) {
+        return res.redirect('404');
+    }
+
+    res.render('details', { cube });
+}
