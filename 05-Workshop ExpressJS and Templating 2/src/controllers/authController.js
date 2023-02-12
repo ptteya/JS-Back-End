e32033622784089982e3652b55ac90b2977b0797
ptteya <petq_1002@abv.bg>
2023-02-12 16:38:55 +0200

    Register user

--- 05-Workshop ExpressJS and Templating 2/src/controllers/authController.js
+++ 05-Workshop ExpressJS and Templating 2/src/controllers/authController.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 
+const authManager = require('../manager/authManager');
+
 router.get('/login', (req, res) => {
     res.render('auth/login');
 });
@@ -8,12 +10,24 @@ router.get('/register', (req, res) => {
     res.render('auth/register');
 });
 
-router.post('/register', () => {
+router.post('/register', async (req, res) => {
     const { username, password, repeatPassword } = req.body;
 
     if (password !== repeatPassword) {
+        return res.render('404');
+    }
+
+    const existingUser = await authManager.getUserByUsername(username);
+
+    if (existingUser) {
         return res.status(404).end();
     }
+
+    const user = await authManager.register(username, password);
+
+    console.log(user);
+
+    res.redirect('/login')
 });
 
 module.exports = router;
