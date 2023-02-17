eeec6b7ce6ae8b8a0665f76c3479731f8b986120
ptteya <petq_1002@abv.bg>
2023-02-17 12:55:54 +0200

    Implement bidder logic

--- 09-Exams/01-Auction House/controllers/auctionController.js
+++ 09-Exams/01-Auction House/controllers/auctionController.js
@@ -12,11 +12,10 @@ router.post('/create', async (req, res) => {
 
     try {
         await auctionService.create(req.user._id, auctionData);
+        res.redirect('/auction/browse');
     } catch (error) {
         res.status(400).render('auction/create', { error: getErrorMessage(error) });
     }
-
-    res.redirect('/auction/browse');
 });
 
 router.get('/browse', async (req, res) => {
@@ -26,10 +25,24 @@ router.get('/browse', async (req, res) => {
 });
 
 router.get('/:auctionId/details', async (req, res) => {
-    const auction = await auctionService.getOne(req.params.auctionId).populate('author');
-    const author = `${auction.author.firstName} ${auction.author.lastName}`
+    const auction = await auctionService.getOne(req.params.auctionId).populate('author').populate('bidder');
+    const author = `${auction.author.firstName} ${auction.author.lastName}`;
 
-    res.render('auction/details', { auction, author });
+    const isAuthor = auction.author._id == req.user?._id;
+    const canBid = !isAuthor && auction.bidder?._id != req.user?._id;
+
+    res.render('auction/details', { auction, author, isAuthor, canBid });
+});
+
+router.post('/:auctionId/bid', async (req, res) => {
+    const { bidAmount } = req.body;
+
+    try {
+        await auctionService.updatePriceAndBidder(req.user._id, req.params.auctionId, Number(bidAmount));
+        res.redirect(`/auction/${req.params.auctionId}/details`);
+    } catch (err) {
+        res.status(400).render('auction/create', { error: getErrorMessage(error) });
+    }
 });
 
 module.exports = router;
--- 09-Exams/01-Auction House/services/auctionService.js
+++ 09-Exams/01-Auction House/services/auctionService.js
@@ -5,3 +5,17 @@ exports.getOne = (auctionId) => Auction.findById(auctionId).lean();
 exports.getAll = () => Auction.find({}).lean();
 
 exports.create = (authorId, auctionData) => Auction.create({ ...auctionData, author: authorId });
+
+exports.updatePriceAndBidder = async (userId, auctionId, bidAmount) => {
+    const auction = await this.getOne(auctionId);
+
+    if (userId == auction.bidder) {
+        throw new Error('You are already the highest bidder');
+    } else if (bidAmount <= auction.price) {
+        throw new Error('The price should be bigger than the current one!');
+    } else if (auction.author == userId) {
+        throw new Error('You cannot bid for your own auction!');
+    }
+
+    return Auction.findByIdAndUpdate(auctionId, { price: bidAmount, bidder: userId }, { runValidators: true });
+};
